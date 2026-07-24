941a8ee30ba5bc9bd772bca64f0998b9723cbb91
Mạnh Thắng <thangmt868@gmail.com>
2026-07-25 00:36:55 +0700

    update: lib shadcn/ui

---
+++ src/components/GlobalStyles/index.tsx
@@ -0,0 +1,8 @@
+import "./GlobalStyles.scss"
+import * as React from "react";
+
+function GlobalStyles({children}: {children: React.ReactNode}) {
+    return children
+}
+
+export default GlobalStyles;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react(), tailwindcss()],
+    server: {
+        port: 3000,
+        proxy: {
+            "/api": {
+                target: "http://localhost:8000",
+                changeOrigin: true,
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, 'src'),
+        }
+    }
 })
